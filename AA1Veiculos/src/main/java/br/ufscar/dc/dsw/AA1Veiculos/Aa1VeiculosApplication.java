3f805b33445ee00b4f9c5294e3aeec8c43db875b
Guilherme Santiago <guilhermesantiago@estudante.ufscar.br>
2025-06-09 16:19:38 -0300

    Inicio do projeto

---
+++ AA1Veiculos/src/main/java/br/ufscar/dc/dsw/AA1Veiculos/Aa1VeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.AA1Veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aa1VeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aa1VeiculosApplication.class, args);
+	}
+
+}
---
+++ AA1Veiculos/src/test/java/br/ufscar/dc/dsw/AA1Veiculos/Aa1VeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.AA1Veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aa1VeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
