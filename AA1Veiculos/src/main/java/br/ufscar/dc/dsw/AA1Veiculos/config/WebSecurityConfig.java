2b4e0e1dd4ff66e2250a1739e71dd2ac275c4d7c
Guilherme Santiago <guilhermesantiago@estudante.ufscar.br>
2025-06-16 19:24:33 -0300

    Adição das configurações do mvc e adição do driver do mysql

--- AA1Veiculos/src/main/java/br/ufscar/dc/dsw/AA1Veiculos/config/WebSecurityConfig.java
+++ AA1Veiculos/src/main/java/br/ufscar/dc/dsw/AA1Veiculos/config/WebSecurityConfig.java
@@ -1,5 +1,35 @@
 package br.ufscar.dc.dsw.AA1Veiculos.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
-    
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests((authz) -> authz
+						.requestMatchers("/error", "/login/**", "/home/**","/js/**").permitAll()
+						.requestMatchers("/css/**", "/image/**", "/webjars/**").permitAll()
+						.anyRequest().authenticated())
+				.formLogin((form) -> form
+						.loginPage("/login")
+						.permitAll())
+				.logout((logout) -> logout
+						.logoutSuccessUrl("/").permitAll());
+
+		return http.build();
+	}
 }
