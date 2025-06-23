054224afcf1eabc197911c76f81fa48676474fc7
GuilhermeWisniewski <114250592+GuilhermeWisniewski@users.noreply.github.com>
2025-06-23 11:13:38 -0300

    Internacionalização da Proposta
    Internacionalização da Proposta e da home

--- AA1Veiculos/src/main/java/br/ufscar/dc/dsw/AA1Veiculos/config/MvcConfig.java
+++ AA1Veiculos/src/main/java/br/ufscar/dc/dsw/AA1Veiculos/config/MvcConfig.java
@@ -19,30 +19,31 @@ import br.ufscar.dc.dsw.AA1Veiculos.conversor.BigDecimalConversor;
 @ComponentScan(basePackages = "br.ufscar.dc.dsw.AA1Veiculos.config")
 public class MvcConfig implements WebMvcConfigurer {
 
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("redirect:/home");
-		registry.addViewController("/home").setViewName("home");
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:/home");
+        registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/login").setViewName("login");
-	}
+    }
 
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(new Locale("pt", "BR"));
-		return slr;
-	}
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("pt", "BR"));
+        return slr;
+    }
 
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-		lci.setParamName("lang");
-		return lci;
-	}
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeChangeInterceptor());
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 	
 	@Override
     public void addFormatters(FormatterRegistry registry) {
