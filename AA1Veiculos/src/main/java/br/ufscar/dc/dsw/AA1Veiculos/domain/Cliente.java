11cf0a7fb607d100e255e0911d314a7998433503
camilaolivdesouza <camilasouza@estudante.ufscar.br>
2025-07-06 19:06:51 -0300

    Revert "Adiciona máscaras funcionais em CNPJ e CPF"
    This reverts commit 50208baa608dfb95666c99e8fb8b94d9c4d0254a.

--- AA1Veiculos/src/main/java/br/ufscar/dc/dsw/AA1Veiculos/domain/Cliente.java
+++ AA1Veiculos/src/main/java/br/ufscar/dc/dsw/AA1Veiculos/domain/Cliente.java
@@ -4,9 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
 import br.ufscar.dc.dsw.AA1Veiculos.validation.UniqueCPF;
 
 @SuppressWarnings("serial")
@@ -16,8 +13,8 @@ public class Cliente extends AbstractEntity<Long> {
 
     @UniqueCPF (message = "{Unique.CPF}")
 	@NotBlank
-	@Size(min = 14, max = 14, message = "{cliente.cpf.invalido}")
-	@Column(nullable = false, unique = true, length = 14)
+	@Size(min = 11, max = 11, message = "{cliente.cpf.invalido}")
+	@Column(nullable = false, unique = true, length = 60)
 	private String cpf;
 
     @NotBlank
@@ -38,8 +35,6 @@ public class Cliente extends AbstractEntity<Long> {
 
     @NotNull
     @Past
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    @Column(nullable = false, columnDefinition = "DATE")
     private LocalDate dataNascimento;
 
     public List<Proposta> getPropostas() {
